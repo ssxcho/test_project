0d21622cf92d788fb6ab05effc3848ae09f6566a
ssxcho <xchton@gmail.com>
2022-08-01 15:30:42 +0400

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+def hamar():
+    a = input("heraxosahamar")
+
+    c = a[0:3]
+
+    if len(a) != 9:
+        print("sxal es gre hamar@")
+    if len(a) == 9 and c == "093":
+        print("esi viva celli hamara")
+    if len(a) == 9 and c == "091":
+        print("esi armenteli hamara")
+    if  len(a) == 9 and c == "096":
+        print("esi ucomi hamara")
+hamar()
